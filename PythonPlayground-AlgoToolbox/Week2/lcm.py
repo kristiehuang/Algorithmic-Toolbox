9d8f4ab965cb3e4e85a11084a3c5d7fe715c65f4
Kristie Huang <kristiehuang01@gmail.com>
2019-07-26 16:29:23 -0700

    Least common multiple

--- PythonPlayground-AlgoToolbox/Week2/lcm.py
+++ PythonPlayground-AlgoToolbox/Week2/lcm.py
@@ -1,15 +1,36 @@
 # Uses python3
 import sys
+#from gcd import gcd
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+# def lcm_naive(a, b):
+#     for l in range(1, a*b + 1):
+#         if l % a == 0 and l % b == 0:
+#             return l
+#
+#     return a*b
+#
+# if __name__ == '__main__':
+#     input = sys.stdin.read()
+#     a, b = map(int, input.split())
+#     print(lcm_naive(a, b))
+def gcd(a, b):
+    current_gcd = 1
+    first = a
+    second = b
+    while (second != 0):
+        first, second = second, first % second
+    current_gcd = first
 
-    return a*b
+    return current_gcd
+
+
+def lcm(a, b):
+    current_gcd = gcd(a, b)
+
+    return a*b / current_gcd
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
-
+    print(lcm(a, b))
