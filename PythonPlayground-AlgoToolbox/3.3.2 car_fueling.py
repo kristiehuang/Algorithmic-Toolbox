2efe26cffdd30fa6b6f018ba85cb58df83d7d8f5
Kristie Huang <kristiehuang01@gmail.com>
2019-08-16 22:04:03 -0700

    7 hours on a freaking problem that doesn't work

--- PythonPlayground-AlgoToolbox/3.3.2 car_fueling.py
+++ PythonPlayground-AlgoToolbox/3.3.2 car_fueling.py
@@ -7,25 +7,30 @@ def compute_min_refills(distance, tank, stops):
     refills = 0
     tankMiles = tank
 
-    while distance > tankMiles:
-        stop = -1
-        for x in stops:
-            if x <= tankMiles:
-                stop = x
-            else:
-                break
 
-        if stop < 0:
-            refills = -1
-            break
-        else:
-            refills+=1
-            tankMiles = stop + tank
+    if distance <= tankMiles:
+        refills = 0
+    else:
+        stop = -1
+        while distance > tankMiles:
+            nextStop = getNextStop(stop, tankMiles, stops)
+
+            print("Stop at", nextStop)
+            if nextStop < 0:
+                break
+            else:
+                refills+=1
+                tankMiles = stop + tank
 
     return refills
 
+def getNextStop(_stop, _tankMiles, _stops):
+    nextStop = -1;
+    for x in _stops:
+        if x <= _tankMiles and x > _stop:
+            nextStop = x
 
-
+    return nextStop
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
