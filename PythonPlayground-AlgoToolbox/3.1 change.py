ad92f60e57cff99628a6f9049f47d92a6b46b7eb
Kristie Huang <kristiehuang01@gmail.com>
2019-08-16 18:39:54 -0700

    Does it work???

--- PythonPlayground-AlgoToolbox/3.1 change.py
+++ PythonPlayground-AlgoToolbox/3.1 change.py
@@ -3,8 +3,15 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+
+    tens = int(m/10)
+    remTen = m % 10
+    fives = int(remTen / 5)
+    remFives = int(remTen % 5)
+
+    change = tens + fives + remFives
+    return change
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
--- PythonPlayground-AlgoToolbox/3.3 car_fueling.py
+++ PythonPlayground-AlgoToolbox/3.3 car_fueling.py
@@ -3,8 +3,61 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+
+    # 100 miles
+    # 30 tank
+    # 4 stops -- 20, 50, 60, 80
+    # 20, 50, 80
+    #
+    # 2 stops -- 20, 60
+
+
+    refills = 0
+    currentStop = 0
+
+    n = len(stops)
+
+    while currentStop <= n:
+
+        lastStop = currentStop
+        #print("current stop distance is",stops[currentStop])
+
+        if currentStop + 1 < n:
+            while (currentStop < n) and (stops[currentStop+1] - stops[lastStop]) <= tank:
+                    currentStop += 1
+                    #print("new stop distance is", stops[currentStop])
+                    if currentStop + 1 == n:
+                        break
+                    #finding stop as forward as you can
+
+        if currentStop == lastStop and stops[currentStop] + tank < distance: #if no possible stop is ahead
+            return -1
+        if currentStop <= n:
+            refills += 1
+
+        if stops[currentStop] + tank >= distance:
+            refills +=1
+            break
+
+    # while stops[currentStop] < distance:
+    #     lastStop = currentStop
+    #     while (stops[lastStop] + tank) < distance and (stops[currentStop-1] - stops[lastStop]) <= tank:
+    #             currentStop += 1
+    #             #finding stop as forward as you can
+    #
+    #     if currentStop == lastStop: #if no possible stop is ahead
+    #         refills = -1
+    #     if stops[currentStop] < distance:
+    #         refills += 1
+
+
+
+
+    return refills
+
+
+
+
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
