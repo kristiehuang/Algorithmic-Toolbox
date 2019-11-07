18c22a653d0ab98648b85ad63f2823aca07451ec
Kristie Huang <kristiehuang01@gmail.com>
2019-11-07 15:09:22 -0800

    Finish 3.2 fractional knapsack

--- PythonPlayground-AlgoToolbox/3.2 fractional_knapsack.py
+++ PythonPlayground-AlgoToolbox/3.2 fractional_knapsack.py
@@ -4,7 +4,26 @@ import sys
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
+    def highest_utility_helper(w, v):
+        existing_max = 0.
+        index = 0
+        for i in range(len(w)):
+            if (w[i] > 0) and (v[i] / w[i]) > existing_max:
+                index = i
+                existing_max = v[i] / w[i]
+        return index
 
+    while capacity > 0:
+        i = highest_utility_helper(weights, values)
+
+        if (capacity > weights[i]):
+            value += values[i]
+            capacity -= weights[i]
+            weights[i] = 0
+        else:
+            value += (capacity * values[i] / weights[i])
+            weights[i] -= capacity
+            capacity = 0
     return value
 
 
@@ -13,5 +32,6 @@ if __name__ == "__main__":
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
+
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
